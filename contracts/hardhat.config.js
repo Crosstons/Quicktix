1c993716961cd7420aec32ecd15d9676c6279afc
kamuikatsurgi <shahkrishang11@gmail.com>
2024-01-29 19:52:24 +0530

    updated hardhat config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.21",
+  },
+  networks: {
+    "lightlink-mainnet": {
+      url: "https://replicator.phoenix.lightlink.io/rpc/v1",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+    "lightlink-testnet": {
+      url: "https://replicator.pegasus.lightlink.io/rpc/v1",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    "base-local": {
+      url: "http://localhost:8545",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
 };
